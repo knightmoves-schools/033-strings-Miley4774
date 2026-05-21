9fa2a805e1586deb5a1cb4174f12b0dbc7f0d12f
Miley john pork <miley.perez@students.msswarriors.org>
2026-05-21 14:19:47 -0500

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+let firstname = "athena" ;
+let jobTitle = "supreme"
+let city  = "brazil"
+
+let newJobPost = Dear ${firstName},
+  Thank you for your interest in ${jobTitle} in ${city} someone will email you
+
+  document.getElementById('result').innerHTML = newJobPost;
