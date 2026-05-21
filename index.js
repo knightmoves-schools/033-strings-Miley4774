95307d23df4510a776a772fcd9c4d99d987f8b04
Miley john pork <miley.perez@students.msswarriors.org>
2026-05-21 14:22:59 -0500

    try again

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let firstname = "athena" ;
-let jobTitle = "supreme" ;
-let city  = "brazil" ;
+let firstName = 'athena' ;
+let jobTitle = 'supreme' ;
+let city  = 'brazil' ;
 
-let newJobPost = Dear ${firstName},
-  Thank you for your interest in ${jobTitle} in ${city} someone will email you
+let newJobPost = `Dear ${firstName},
+  Thank you for your interest in ${jobTitle} in ${city} someone will email you`;
 
   document.getElementById('result').innerHTML = newJobPost;
